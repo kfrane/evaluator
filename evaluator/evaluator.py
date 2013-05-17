25091e393735cd69d93925bb3e0c8a5de6fb32b4
kfrane <frane.kurtovic@gmail.com>
2013-05-17 20:47:38 +0200

    Added memory and time limit to evaluator.
    It doesn't yet notify that the memory limit has been exceeded,
    just kills the process.

--- evaluator/evaluator.py
+++ evaluator/evaluator.py
@@ -5,6 +5,8 @@ import os
 import sys
 import shutil
 import getopt
+import threading
+import time
 
 def copy_if_needed(source, dest):
   last_slash = source.rfind('/')
@@ -44,11 +46,42 @@ def parse_args():
     sys.exit(1)
   return (memory_limit, time_limit, args[0], args[1:])
 
+start_time = 0
+class Attach(threading.Thread):
+  def __init__(self, container, cmd_in_container, cmd_params):
+    threading.Thread.__init__(self)
+    self.container = container
+    self.cmd_in_container = cmd_in_container
+    self.cmd_params = cmd_params
+
+  def run(self):
+    global start_time
+    start_time = time.time()
+    self.container.attach('ALL', self.cmd_in_container, *self.cmd_params)
+
+def force_kill(container):
+  prev_pid = None
+  while container.running:
+    init_pid = container.init_pid
+    if init_pid == prev_pid:
+      # if the process isn't killed we will give it 100 ms before another kill
+      time.sleep(0.1)
+      prev_pid = None
+      continue
+    if init_pid <= 1:
+      return
+    try:
+      os.kill(init_pid, signal.SIGKILL)
+    except:
+      pass
+    prev_pid = init_pid
+
 def main():
   # some configs
   container_name = 'sandbox'
   container_paths = '/var/lib/lxc/'
   rootfs = container_paths + container_name + '/rootfs'
+  cgroup_mem = ['memory.limit_in_bytes', 'memory.memsw.limit_in_bytes']
 
   # parse command line args
   memory_limit, time_limit, cmd_to_run, cmd_params = parse_args()
@@ -62,14 +95,32 @@ def main():
     container.stop()
     print ("Container", container_name, "won't run")
     return
-
+  print('memory limit', memory_limit)
+  for limit in cgroup_mem:
+    if memory_limit == None:
+      break
+    if container.set_cgroup_item(limit, memory_limit) == False:
+      print(container.get_cgroup_item(limit))
+      print("Aborting, couldn't set memory limits for container.")
+      container.stop()
+      sys.exit(1)
+  ign = input()
   tmp_path = rootfs + '/tmp'
   cmd_in_container = '/tmp/' + copy_if_needed(cmd_to_run, tmp_path)
   print ("Cmd in container", cmd_in_container)
-  container.attach('ALL', cmd_in_container, *cmd_params)
+  attach_thread = Attach(container, cmd_in_container, cmd_params)
+  attach_thread.start()
+  print('Immediately after attach_thead.start()')
+  attach_thread.join(float(time_limit) / 1000.0)
+  time_elapsed = time.time() - start_time
+  time_limit_exceeded = False
+  if attach_thread.isAlive():
+    time_limit_exceeded = True
 
-  print('Immediately after attach')
   container.stop()
+  container.wait("STOPPED", 1)
+  if container.running:
+    force_kill(container)
 
 if __name__ == "__main__":
   main()
